202d46d146f66fdabdffdfcc2cdbab0529149947
jllee000 <jllee000@naver.com>
2025-08-19 01:32:09 +0900

    setting: 화면 ui 세팅

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-
+import './index.css' 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-       안녕
-      </header>
+      <div className="h-[50rem] w-[26rem] bg-blue-500 text-white flex items-center justify-center text-2xl">
+       자 핀백 앱잼 시작~오늘은 7월 7일임~
+      </div>
     </div>
   );
 };
