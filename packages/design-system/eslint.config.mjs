436e5a39db0ba4c4c52d6013efc36361550a342d
constantly-dev <rntqhdl10@naver.com>
2025-08-21 03:24:49 +0900

    setting: storybook default 설정

--- packages/design-system/eslint.config.mjs
+++ packages/design-system/eslint.config.mjs
@@ -1,4 +1,26 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import config from '@pinback/eslint-config/react-internal';
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+  ...storybook.configs['flat/recommended'],
+  config,
+  {
+    files: ['**/*.js', '**/*.ts'],
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      'public',
+      'public/**',
+      'public/**/*',
+      'icons/**/*',
+    ],
+    rules: {
+      'no-undef': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+];
