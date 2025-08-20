2d65fb1b3554dca42f255ec974d5373478739ca8
jjangminii <wjdals030328@catholic.ac.kr>
2025-08-21 02:43:38 +0900

    setting: iconNames 자동 프리티어 추가

--- packages/design-system/src/icons/iconNames.ts
+++ packages/design-system/src/icons/iconNames.ts
@@ -1,3 +1,7 @@
 // 이 파일은 자동 생성 파일입니다. (직접 수정 금지)
-export const iconNames = ["dotori"] as const;
-export type IconName = typeof iconNames[number];
+export const iconNames = [
+  'dotori',
+  'down_icon',
+  'ic_arrow_down_white',
+] as const;
+export type IconName = (typeof iconNames)[number];
