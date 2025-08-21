eb7349fd951154954c0669b7e7646c7c048da6a0
이진혁 <rntqhdl10@naver.com>
2025-08-22 00:14:24 +0900

    Setting(design-system): storybook 설정 및 chromatic 배포 (#14)
    * setting: storybook default 설정
    
    * setting: pds introduction mdx 작성
    
    * setting: storybook eslint config 변경
    
    * setting: ds package script 추가
    
    * setting: design-system turbo.json 추가 설정
    
    * chore: 불필요한 파일 제거
    
    * setting: chromatic 의존성 추가
    
    * setting: chromatic storybook publish workflow yml 작성
    
    * fix: storybook build script 커스텀한 내용 yml에도 반영
    
    * setting: 예시 button story 추가
    
    * fix: button 예시 컴포넌트에서 default export문 추가
    
    * setting: upgrade storybook version
    
    * setting: preview matchers에 text 추가
    
    * chore: update pnpm-lock
    
    * setting: setting.json에 tailwind css config file 추가

--- packages/design-system/src/components/button/Button.tsx
+++ packages/design-system/src/components/button/Button.tsx
@@ -1,5 +1,13 @@
-const Button = () => {
-  return <div>Button</div>;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Button = ({ children, ...props }: ButtonProps) => {
+  return (
+    <button type="button" {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
