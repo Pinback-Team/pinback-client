8a1f4a545cfc843f08dce8bbb652fff5620dc25f
jllee000 <jllee000@naver.com>
2025-08-22 19:26:04 +0900

    feat: 활성/비활성 스타일 분기

--- packages/design-system/src/components/badge/Badge.tsx
+++ packages/design-system/src/components/badge/Badge.tsx
@@ -1,14 +1,43 @@
+import { cva } from 'class-variance-authority';
+import { useState } from 'react';
 export interface BadgeProps {
   text: string;
   countNum?: number;
 }
+const BadgeTxtStyleVariants = cva('sub3-b', {
+  variants: {
+    click: {
+      true: 'text-font-black-1',
+      false: 'text-font-ltgray-4',
+    } as const,
+  },
+  defaultVariants: {
+    click: false,
+  },
+});
+const BadgeStyleVariants = cva(
+  'text-white-bg sub5-sb rounded-[0.4rem] px-[0.8rem] py-[0.4rem]',
+  {
+    variants: {
+      click: {
+        true: 'bg-main500',
+        false: 'bg-gray300',
+      } as const,
+    },
+    defaultVariants: {
+      click: false,
+    },
+  }
+);
 const Badge = ({ text, countNum }: BadgeProps) => {
+  const [isClick, setIsClick] = useState(false);
   return (
-    <div className="flex cursor-pointer items-center justify-center gap-[0.8rem]">
-      <span className="sub3-sb">{text}</span>
-      <span className="bg-main500 text-white-bg sub5-sb rounded-[0.4rem] px-[0.8rem] py-[0.4rem]">
-        {countNum}
-      </span>
+    <div
+      className="flex cursor-pointer items-center justify-center gap-[0.8rem]"
+      onClick={() => setIsClick(true)}
+    >
+      <span className={BadgeTxtStyleVariants({ click: isClick })}>{text}</span>
+      <span className={BadgeStyleVariants({ click: isClick })}>{countNum}</span>
     </div>
   );
 };
