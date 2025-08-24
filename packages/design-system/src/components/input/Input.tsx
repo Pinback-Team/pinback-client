3cbf52ef7637f45febd5944dde7521201c3ff4e8
KIM JEONGMIN <163228804+jjangminii@users.noreply.github.com>
2025-08-24 16:24:01 +0900

    Feat(design-system): input 공통 컴폰넌트, 스토리북 구현 (#22)
    * feat: input 세팅
    
    * feat: 고정폭 삭제
    
    * chore: 에시 코드 삭제
    
    * feat: 스토리북 파일 생성
    
    * feat: 스토리북 작성
    
    * feat: 스토리북 테스트 패키지 추가 및 입력 스토리에서 ref 숨김
    
    * feat: createRequire을 사용하여 모듈 경로 해결 기능 추가
    
    * feat: Input 스토리에서 ref 타입 숨김 및 타입 정의 개선
    
    * feat: 'Error' 스토리 이름 변경 및 ControlledExample 컴포넌트로 리팩토링

---
+++ packages/design-system/src/components/input/Input.tsx
@@ -0,0 +1,53 @@
+import { cva } from 'class-variance-authority';
+import { InputHTMLAttributes, Ref } from 'react';
+import { cn } from '../../lib';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  ref: Ref<HTMLInputElement>;
+  isError?: boolean;
+  helperText?: string;
+}
+
+const inputBorderVariants = cva(
+  'w-full h-[4.4rem] rounded-[4px] border box-border body4-r px-[0.8rem] py-[1.2rem] transition-colors ',
+  {
+    variants: {
+      isError: {
+        true: 'border-error focus-within:border-error',
+        false: 'border-gray200 focus-within:border-main400',
+      },
+    },
+    defaultVariants: {
+      isError: false,
+    },
+  }
+);
+
+const Input = ({
+  ref,
+  isError,
+  className,
+  helperText,
+  ...props
+}: InputProps) => {
+  return (
+    <div className="flex flex-col gap-[0.5rem]">
+      <div className={cn(inputBorderVariants({ isError }), className)}>
+        <input
+          ref={ref}
+          className="placeholder-font-gray-3 w-full focus:outline-none"
+          aria-invalid={isError}
+          {...props}
+        />
+      </div>
+
+      {isError && helperText && (
+        <div className="flex items-center">
+          <p className="text-error body4-r">{helperText}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
