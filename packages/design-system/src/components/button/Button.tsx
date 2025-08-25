50d53aec5f26c00ce2ae285d9b777d5dc105cf39
constantly-dev <rntqhdl10@naver.com>
2025-08-25 21:57:19 +0900

    feat: button 공통 컴포넌트 구현

--- packages/design-system/src/components/button/Button.tsx
+++ packages/design-system/src/components/button/Button.tsx
@@ -1,10 +1,50 @@
+import { cn } from '@/lib';
+import { cva } from 'class-variance-authority';
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  variant?: 'primary' | 'secondary';
+  size?: 'small' | 'medium' | 'large';
+  isDisabled?: boolean;
 }
 
-const Button = ({ children, ...props }: ButtonProps) => {
+const buttonVariants = cva(
+  'cursor-pointer rounded-[0.4rem] disabled:cursor-not-allowed w-full',
+  {
+    variants: {
+      variant: {
+        primary:
+          'bg-gray900 text-white-bg hover:bg-gray800 active:bg-gray900 disabled:bg-gray200 disabled:text-font-ltgray-4',
+        secondary:
+          'bg-white-bg text-font-black-1 hover:bg-gray200 active:bg-white-bg disabled:bg-gray200 disabled:text-font-ltgray-4 outline outline-gray200',
+      },
+      size: {
+        small: 'py-[0.8rem] sub5-sb',
+        medium: 'py-[1.2rem] sub5-sb',
+        large: 'py-[1.5rem] sub3-sb',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'medium',
+    },
+  }
+);
+
+const Button = ({
+  variant = 'primary',
+  size,
+  children,
+  className,
+  isDisabled = false,
+  ...props
+}: ButtonProps) => {
   return (
-    <button type="button" {...props}>
+    <button
+      type="button"
+      className={cn(buttonVariants({ variant, size }), className)}
+      disabled={isDisabled}
+      {...props}
+    >
       {children}
     </button>
   );
