6d68dc1746282437d4d194de1dd8b0f8878a64f4
jjangminii <wjdals030328@catholic.ac.kr>
2025-08-25 23:20:34 +0900

    refactor: 파일 이름 변경

--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from './button/Button';
-export { Switch } from './switch/switch';
+export { Switch } from './switch/Switch';
 export { default as Input } from './input/Input';
 export { Progress } from './progress/Progress';
