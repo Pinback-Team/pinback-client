a70d51cbf96ce9473a961a17c186b630882ad0be
jllee000 <jllee000@naver.com>
2025-08-25 19:01:54 +0900

    feat: 상태에 따른 색상 분기

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <DateTime type="time" value="2023.10.01" />
+      <DateTime state="default" type="time" value="2023.10.01" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- packages/design-system/src/components/dateTime/DateTime.tsx
+++ packages/design-system/src/components/dateTime/DateTime.tsx
@@ -1,9 +1,52 @@
 import { useRef, useState, useLayoutEffect, type ChangeEvent } from 'react';
-
+import { cva } from 'class-variance-authority';
 interface DateTimeProps {
+  state: 'default' | 'disabled' | 'error';
   type: 'date' | 'time';
   value?: string; // 초기값(optional)
 }
+const dateTimeBoxStyles = cva(
+  `flex w-[12rem] items-center gap-[0.8rem] rounded-[0.4rem] border px-[0.8rem] py-[1.2rem]`,
+  {
+    variants: {
+      state: {
+        default: 'border-gray200 bg-white-bg ',
+        disabled: 'border-gray200 bg-gray200',
+        error: 'border-error bg-white-bg ',
+      },
+    },
+    defaultVariants: {
+      state: 'default',
+    },
+  }
+);
+const dateTimeLabelStyles = cva(`caption1-sb`, {
+  variants: {
+    state: {
+      default: 'text-font-black-1',
+      disabled: 'text-font-ltgray-4',
+      error: 'text-font-black-1',
+    },
+  },
+  defaultVariants: {
+    state: 'default',
+  },
+});
+const dateTimeTxtStyles = cva(
+  `body3-r m-0 w-[7rem] overflow-hidden outline-none`,
+  {
+    variants: {
+      state: {
+        default: 'text-font-gray-3',
+        disabled: 'text-font-ltgray-4',
+        error: 'text-error',
+      },
+    },
+    defaultVariants: {
+      state: 'default',
+    },
+  }
+);
 
 export default function DateTime({ type, value = '' }: DateTimeProps) {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -71,14 +114,14 @@ export default function DateTime({ type, value = '' }: DateTimeProps) {
   }, [input]);
 
   return (
-    <div className="border-gray200 bg-white-bg flex w-[12rem] items-center gap-[0.8rem] rounded-[0.4rem] border px-[0.8rem] py-[1.2rem]">
-      <span className="caption1-sb text-font-black-1">
+    <div className={dateTimeBoxStyles({ state: 'default' })}>
+      <span className={dateTimeLabelStyles({ state: 'default' })}>
         {type === 'date' ? '날짜' : '시간'}
       </span>
       <input
         ref={inputRef}
         type="text"
-        className="body3-r text-font-gray-3 m-0 w-[7rem] overflow-hidden outline-none"
+        className={dateTimeTxtStyles({ state: 'default' })}
         value={input}
         onChange={handleChange}
         placeholder={type === 'date' ? 'YYYY.MM.DD' : 'HH:MM'}
