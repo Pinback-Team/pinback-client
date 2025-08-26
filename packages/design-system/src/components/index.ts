1dc8d24897cc361b7621c53f29694dab388948b8
이진혁 <rntqhdl10@naver.com>
2025-08-27 02:38:24 +0900

    Feat(design-system): Card 컴포넌트 구현 (#40)
    * setting: font pretendard 추가
    
    * feat: base card 컴포넌트 구현
    
    * feat: bookmark card 컴포넌트 구현
    
    * feat: remind card 컴포넌트 구현
    
    * feat: card 컴포넌트 구현
    
    * feat: card story 작성
    
    * chore: bookmark naming에 card 추가
    
    * setting: design-system d.ts 추가
    
    * chore: card story 정리
    
    * feat: card no image case 추가
    
    * feat: margin 변경 및 story description 추가
    
    * refactor: 더보기 버튼 aria-label 추가
    
    * refactor: card 2개 type never로 확실한 타입 분기 적용

--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -1,8 +1,9 @@
-export { default as Button } from './button/Button';
-export { Switch } from './switch/Switch';
 export { default as Badge } from './badge/Badge';
-export { default as Input } from './input/Input';
+export { default as Button } from './button/Button';
+export { default as Card } from './card/Card';
 export { default as Chip } from './chip/Chip';
+export { default as Input } from './input/Input';
 export { default as Level } from './level/Level';
-export { Textarea } from './textarea/Textarea';
 export { Progress } from './progress/Progress';
+export { Switch } from './switch/Switch';
+export { Textarea } from './textarea/Textarea';
