bb59c9d8120f55bb9217bfaedbe95938824e0241
이진혁 <rntqhdl10@naver.com>
2025-08-27 15:27:11 +0900

    Setting(project): router 초기 설정 (#46)
    * setting: react router dom 의존성 추가
    
    * setting: routes 절대 경로 추가
    
    * setting: dashboard 페이지 컴포넌트 추가
    
    * setting: route config constants 설정
    
    * setting: outlet 사용 layout 추가

---
+++ apps/client/src/routes/routesConfig.ts
@@ -0,0 +1,18 @@
+export const ROUTES_CONFIG = {
+  remind: {
+    title: '리마인드',
+    path: '/',
+  },
+  myBookmarks: {
+    title: '나의 북마크',
+    path: '/my-bookmarks',
+  },
+  category: {
+    title: '카테고리',
+    path: (id: string) => `/category/${id}`,
+  },
+  level: {
+    title: '레벨',
+    path: '/level',
+  },
+};
