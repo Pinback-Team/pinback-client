878d41c44f6a303f584fef234cc3c7deaaeff940
jjangminii <wjdals030328@catholic.ac.kr>
2025-09-05 02:11:27 +0900

    chore: 주석 제거

--- apps/client/src/pages/level/Level.tsx
+++ apps/client/src/pages/level/Level.tsx
@@ -1,29 +1,9 @@
 import TreeStatusCard from './components/TreeStatusCard';
 import LevelInfoCard from './components/LevelInfoCard';
-import { Icon } from '@pinback/design-system/icons';
 
 const Level = () => {
   return (
     <div className="bg-secondary flex flex-col gap-[2rem] p-[1rem]">
-      <Icon
-        name={'tooltip_1'}
-        width={46}
-        height={46}
-        className="rounded-[0.8rem]"
-      />
-      <Icon
-        name={'ic_plus'}
-        width={46}
-        height={46}
-        className="rounded-[0.8rem]"
-      />
-      <Icon
-        name={'ic_plus'}
-        width={46}
-        height={46}
-        className="rounded-[0.8rem]"
-      />
-      <Icon name={'tooltip_4'} className="rounded-[0.8rem]" />
       <LevelInfoCard />
       <TreeStatusCard acorns={0} />
       <TreeStatusCard acorns={1} />
