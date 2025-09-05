152c232628c89a3ec9df0f95eb590731770e7f28
jjangminii <wjdals030328@catholic.ac.kr>
2025-09-05 17:19:08 +0900

    chore: 주석 제거

--- apps/client/src/pages/level/Level.tsx
+++ apps/client/src/pages/level/Level.tsx
@@ -1,21 +1,5 @@
-import TreeStatusCard from './components/TreeStatusCard';
-import LevelInfoCard from './components/LevelInfoCard';
-
 const Level = () => {
-  return (
-    <div className="bg-secondary flex flex-col gap-[2rem] p-[1rem]">
-      <LevelInfoCard />
-      <TreeStatusCard acorns={0} />
-      <TreeStatusCard acorns={1} />
-      <TreeStatusCard acorns={2} />
-      <TreeStatusCard acorns={3} />
-      <TreeStatusCard acorns={4} />
-      <TreeStatusCard acorns={5} />
-      <TreeStatusCard acorns={6} />
-      <TreeStatusCard acorns={7} />
-      <TreeStatusCard acorns={987987} />
-    </div>
-  );
+  return <div className="bg-secondary flex flex-col gap-[2rem] p-[1rem]"></div>;
 };
 
 export default Level;
