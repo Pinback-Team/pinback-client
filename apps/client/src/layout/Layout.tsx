1969490b24cf011d0559331941050a60922c4863
jllee000 <jllee000@naver.com>
2025-09-06 15:42:09 +0900

    feat: 큰 플로우 스텝 분기

--- apps/client/src/layout/Layout.tsx
+++ apps/client/src/layout/Layout.tsx
@@ -1,16 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import OptionsMenuButton from '@shared/components/optionsMenuButton/OptionsMenuButton';
 
 const Layout = () => {
   return (
     <>
       {/* TODO: 필요시 레이아웃 추가 */}
       {/* TODO: 사이드바 추가 */}
-      <OptionsMenuButton
-        onEdit={function (): void {}}
-        onDelete={function (): void {}}
-      ></OptionsMenuButton>
-
       <Outlet />
     </>
   );
--- apps/client/src/pages/onBoarding/components/funnel/MainCard.tsx
+++ apps/client/src/pages/onBoarding/components/funnel/MainCard.tsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import StoryStep from './StoryStep';
 import AlarmStep from './AlarmStep';
-
+import MacStep from './MacStep';
+import FinalStep from './FinalStep';
 const stepProgress = [{ progress: 30 }, { progress: 60 }, { progress: 100 }];
 
 const variants = {
@@ -21,12 +22,29 @@ const variants = {
 const MainCard = () => {
   const [step, setStep] = useState(0);
   const [direction, setDirection] = useState(0);
-
+  const renderStep = () => {
+    switch (step) {
+      case 0:
+      case 1:
+      case 2:
+        return <StoryStep step={step as 0 | 1 | 2} />;
+      case 3:
+        return <AlarmStep />;
+      case 4:
+        return <MacStep />;
+      case 5:
+        return <FinalStep />;
+      default:
+        return <FinalStep />;
+    }
+  };
   const nextStep = () => {
-    if (step < 3) {
-      // 0,1,2 → story, 3 → alarm
+    if (step < 5) {
       setDirection(1);
       setStep((prev) => prev + 1);
+    }
+    if (step === 5) {
+      window.location.href = '/';
     }
   };
 
@@ -39,7 +57,6 @@ const MainCard = () => {
 
   return (
     <div className="bg-white-bg flex h-[54.8rem] w-[63.2rem] flex-col items-center justify-between overflow-hidden rounded-[2.4rem] pt-[3.2rem]">
-      {/* ProgressBar는 story 단계에서만 보여줌 */}
       {step < 3 && (
         <Progress
           value={stepProgress[step].progress}
@@ -58,28 +75,30 @@ const MainCard = () => {
             animate="center"
             exit="exit"
             transition={{ duration: 0.4 }}
-            className="absolute flex flex-col items-center"
+            className="flex h-full flex-col items-center"
           >
-            {step < 3 ? <StoryStep step={step as 0 | 1 | 2} /> : <AlarmStep />}
+            {renderStep()}
           </motion.div>
         </AnimatePresence>
       </div>
 
       <div className="mb-[4.8rem] mt-[1.2rem] flex w-full justify-between px-[3.2rem]">
+        {step < 4 && (
+          <Button
+            variant="primary"
+            size="medium"
+            isDisabled={step === 0}
+            className="w-[4.8rem]"
+            onClick={prevStep}
+          >
+            이전
+          </Button>
+        )}
         <Button
           variant="primary"
           size="medium"
-          isDisabled={step === 0}
-          className="w-[4.8rem]"
-          onClick={prevStep}
-        >
-          이전
-        </Button>
-        <Button
-          variant="primary"
-          size="medium"
-          isDisabled={step === 3} // 마지막은 alarmStep
-          className="w-[4.8rem]"
+          isDisabled={step === 6}
+          className="ml-auto w-[4.8rem]"
           onClick={nextStep}
         >
           다음
