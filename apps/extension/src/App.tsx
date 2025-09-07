3632099b418e6e6419be1cce03601f943637be1f
jllee000 <jllee000@naver.com>
2025-09-07 15:05:12 +0900

    feat: 익스텐션 화면 초기 세팅

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <div className="flex h-[50rem] w-[26rem] items-center justify-center bg-blue-500 text-2xl text-white">
+      <div className="bg-main500 flex h-[54.4rem] w-[31.2rem] items-center justify-center text-2xl text-white">
         자 핀백 앱잼 시작~오늘은 7월 7일임~
       </div>
     </div>
