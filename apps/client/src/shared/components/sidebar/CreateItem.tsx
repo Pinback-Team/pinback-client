0a7f9615554b688fc59e3ee7e5aa7ed9698f24b8
jjangminii <wjdals030328@catholic.ac.kr>
2025-09-10 05:35:37 +0900

    feat: 단일 button으로 전환

--- apps/client/src/shared/components/sidebar/CreateItem.tsx
+++ apps/client/src/shared/components/sidebar/CreateItem.tsx
@@ -8,22 +8,20 @@ interface CreateItemProps {
 
 export default function CreateItem({ onClick }: CreateItemProps) {
   return (
-    <div
-      role="button"
+    <button
+      type="button"
       className={cn(
         'bg-white-bg flex h-[3.6rem] w-full gap-[0.8rem] rounded-[0.4rem] p-[0.8rem]',
         'items-center transition-colors'
       )}
       onClick={onClick}
     >
-      <button type="button">
-        <Icon
-          name={'ic_plus'}
-          aria-hidden
-          className={cn('h-[1.6rem] w-[1.6rem]')}
-        />
-      </button>
+      <Icon
+        name={'ic_plus'}
+        aria-hidden
+        className={cn('h-[1.6rem] w-[1.6rem]')}
+      />
       <p className={'body4-r text-main500'}>카테고리 추가</p>
-    </div>
+    </button>
   );
 }
