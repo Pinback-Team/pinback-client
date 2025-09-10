f87db9d2a99da467d85fd45e1e08b8595380fb31
constantly-dev <rntqhdl10@naver.com>
2025-09-11 00:39:19 +0900

    feat: myBookmark page layout 구현

--- apps/client/src/pages/myBookmark/MyBookmark.tsx
+++ apps/client/src/pages/myBookmark/MyBookmark.tsx
@@ -1,5 +1,47 @@
+import { REMIND_MOCK_DATA } from "@pages/remind/constants";
+import { Badge, Card } from "@pinback/design-system/ui";
+import { useState } from "react";
+
 const MyBookmark = () => {
-  return <div>MyBookmark</div>;
+  const [activeBadge, setActiveBadge] = useState('all');
+
+  const handleBadgeClick = (badgeType: string) => {
+    setActiveBadge(badgeType);
+  };
+
+  return (
+    <div className="flex flex-col pl-[8rem] py-[5.2rem]">
+      <p className="head3">리마인드</p>
+      <div className="mt-[3rem] flex gap-[2.4rem]">
+        <Badge
+          text="전체보기"
+          countNum={5}
+          onClick={() => handleBadgeClick('all')}
+          isActive={activeBadge === 'all'}
+        />
+        <Badge
+          text="안 읽음"
+          countNum={10}
+          onClick={() => handleBadgeClick('notRead')}
+          isActive={activeBadge === 'notRead'}
+        />
+      </div>
+
+      <div className="scrollbar-hide mt-[2.6rem] flex flex-wrap gap-[1.6rem] overflow-y-auto scroll-smooth max-w-[104rem]">
+        {/* TODO: API 연결 후 수정 */}
+        {REMIND_MOCK_DATA.map((data) => (
+          <Card
+            key={data.id}
+            type="bookmark"
+            title={data.title}
+            content={data.content}
+            category={data.category}
+            date="2024.08.15"
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyBookmark;
