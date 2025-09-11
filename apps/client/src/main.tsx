72b3fe87e40581c100aa594a1edc533f7dbf9611
constantly-dev <rntqhdl10@naver.com>
2025-09-11 13:46:13 +0900

    setting: query dev tools client 추가

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import getQueryClient from '@shared/apis/query/getQueryClient.ts';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = getQueryClient();
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
