feb4d435c852128207933b7a3b80deaf0b6214a2
constantly-dev <rntqhdl10@naver.com>
2025-09-11 19:10:40 +0900

    feat: 카테고리 생성 api axios 함수 구현

--- apps/client/src/shared/components/sidebar/apis/axios.ts
+++ apps/client/src/shared/components/sidebar/apis/axios.ts
@@ -4,3 +4,10 @@ export const getDashboardCategories = async () => {
   const { data } = await apiRequest.get('/api/v1/categories/dashboard');
   return data;
 };
+
+export const postCategory = async (categoryName: string) => {
+  const response = await apiRequest.post('/api/v1/categories', {
+    categoryName,
+  });
+  return response;
+};
