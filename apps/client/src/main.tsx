607cdb0c04063abb8c5a58ea385359b64e904e77
constantly-dev <rntqhdl10@naver.com>
2025-09-11 13:24:11 +0900

    setting: query provider setting

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import getQueryClient from '@shared/apis/query/getQueryClient.ts';
+
+const queryClient = getQueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
