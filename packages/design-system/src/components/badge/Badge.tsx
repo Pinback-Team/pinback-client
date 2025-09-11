6dca89e188c774d3124eec9a55a97a5ddde3bf73
이진혁 <rntqhdl10@naver.com>
2025-09-11 14:34:07 +0900

    Feat(client): 대시보드 레이아웃 구현 (#66)
    * chore: remind card mock data 추가
    
    * feat: badge interface 변경
    
    * feat: scrollbar hide class 추가
    
    * feat: remind card page 구현
    
    * feat: sidebar 추가 및 전체 레이아웃 구현
    
    * feat: remind card row당 4개로 제한
    
    * feat: myBookmark page layout 구현
    
    * chore: myBookmark page title 수정
    
    * fix: badge story onClick optional 변경으로 인한 오류 해결

--- packages/design-system/src/components/badge/Badge.tsx
+++ packages/design-system/src/components/badge/Badge.tsx
@@ -1,43 +1,50 @@
 import { cva } from 'class-variance-authority';
-import { useState } from 'react';
 export interface BadgeProps {
   text: string;
   countNum?: number;
+  isActive: boolean;
+  onClick?: () => void;
 }
+
 const BadgeTxtStyleVariants = cva('sub3-b', {
   variants: {
-    click: {
+    active: {
       true: 'text-font-black-1',
       false: 'text-font-ltgray-4',
     } as const,
   },
   defaultVariants: {
-    click: false,
+    active: false,
   },
 });
+
 const BadgeStyleVariants = cva(
   'text-white-bg sub5-sb rounded-[0.4rem] px-[0.8rem] py-[0.4rem]',
   {
     variants: {
-      click: {
+      active: {
         true: 'bg-main500',
         false: 'bg-gray300',
       } as const,
     },
     defaultVariants: {
-      click: false,
+      active: false,
     },
   }
 );
-const Badge = ({ text, countNum }: BadgeProps) => {
-  const [isClick, setIsClick] = useState(false);
+
+const Badge = ({ text, countNum, isActive, onClick }: BadgeProps) => {
   return (
     <div
       className="flex cursor-pointer items-center justify-center gap-[0.8rem]"
-      onClick={() => setIsClick(true)}
+      onClick={onClick}
     >
-      <span className={BadgeTxtStyleVariants({ click: isClick })}>{text}</span>
-      <span className={BadgeStyleVariants({ click: isClick })}>{countNum}</span>
+      <span className={BadgeTxtStyleVariants({ active: isActive })}>
+        {text}
+      </span>
+      <span className={BadgeStyleVariants({ active: isActive })}>
+        {countNum}
+      </span>
     </div>
   );
 };
