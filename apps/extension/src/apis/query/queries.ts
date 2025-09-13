d2dd8381cc1e10fe3c469db1e5abaae639b8ef08
jllee000 <jllee000@naver.com>
2025-09-13 14:35:43 +0900

    feat: api 쿼리 분기 제거

--- apps/extension/src/apis/query/queries.ts
+++ apps/extension/src/apis/query/queries.ts
@@ -4,36 +4,18 @@ import { postArticle, PostArticleRequest,postSignup, postSignupRequest, getCateg
 export const usePostArticle = () => {
   return useMutation({
     mutationFn: (data: PostArticleRequest) => postArticle(data),
-    onSuccess: (data) => {
-      console.log("저장 성공:", data);
-    },
-    onError: (error) => {
-      console.error("저장 실패:", error);
-    },
   });
 };
 
 export const usePostSignup = () => {
   return useMutation({
-    mutationFn: (data: postSignupRequest) => postSignup(data),
-    onSuccess: (data) => {
-      console.log("회원가입 성공:", data);
-    },
-    onError: (error) => {
-      console.error("회원가입 실패:", error);
-    },
+    mutationFn: (data: postSignupRequest) => postSignup(data)
   });
 }
 
 export const usePostCategories = () => {
   return useMutation({
     mutationFn: (data: postCategoriesRequest) => postCategories(data),
-    onSuccess: (data) => {
-      console.log("카테고리 저장", data);
-    },
-    onError: (error) => {
-      console.error("카테고리 저장 실패", error);
-    },
   });
 }
 export const useGetCategoriesExtension = () => {
@@ -61,12 +43,6 @@ export const useGetArticleSaved = (url:string) => {
 export const usePutArticle = () => {
   return useMutation({
     mutationFn: ({ articleId, data }: { articleId: number; data: PutArticleRequest }) =>
-      putArticle(articleId, data),
-    onSuccess: (data) => {
-      console.log("아티클 수정 성공:", data);
-    },
-    onError: (error) => {
-      console.error("아티클 수정 실패:", error);
-    },
+      putArticle(articleId, data)
   });
 };
