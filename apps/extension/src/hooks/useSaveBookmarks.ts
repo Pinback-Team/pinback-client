84e6148b6506b473b8d681bb915b06ddbf923706
jllee000 <jllee000@naver.com>
2025-09-14 02:34:39 +0900

    feat: 익스텐션 배포 전 TODO 확인

--- apps/extension/src/hooks/useSaveBookmarks.ts
+++ apps/extension/src/hooks/useSaveBookmarks.ts
@@ -39,8 +39,7 @@ export const useSaveBookmark = () => {
           console.log('크롬 북마크바에 저장 완료: ', newBookmark);
         }
       );
-
-    //  TODO: window.close();
+      window.close();
     } catch (error) {
       console.error('저장 중 오류:', error);
     }
