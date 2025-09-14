a4c18669acae0b26c2af990e77bafa3c258339b1
jjangminii <wjdals030328@catholic.ac.kr>
2025-09-14 15:09:04 +0900

    feat: 랜딩 유아이

---
+++ apps/landing/src/components/FeatureReminderSection.tsx
@@ -0,0 +1,25 @@
+import Lottie from 'lottie-react';
+import lightbulb_chippi_final from '../assets/3_bell.json';
+
+const FeatureReminderSection = () => {
+  return (
+    <section className="flex h-dvh items-center justify-center bg-white">
+      <div className="flex items-center gap-[8rem]">
+        {/* 이미지 영역 (로띠) */}
+        <div className="flex items-center justify-center">
+          <Lottie animationData={lightbulb_chippi_final} loop autoplay />
+        </div>
+        {/* 텍스트 영역 */}
+        <div className="flex flex-col items-end gap-[2.4rem] text-right">
+          <p className="head1">잊지 않도록 리마인드</p>
+          <p className="sub2-sb text-font-gray-3">
+            첫 실행 시 리마인드 추가를 설정해 놓치지 않고 <br />
+            읽을 수 있어요
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeatureReminderSection;
---
+++ apps/landing/src/components/FinalCTASection.tsx
@@ -0,0 +1,31 @@
+import Lottie from 'lottie-react';
+import Chippiface from '../assets/5_chippiface.json';
+import { Button } from '@pinback/design-system/ui';
+
+const FinalCTASection = () => {
+  return (
+    <section className="bg-white-bg flex h-dvh w-full flex-col items-center justify-center gap-[6.4rem] overflow-hidden px-[17.2rem] text-center">
+      <Lottie
+        animationData={Chippiface}
+        loop
+        autoplay
+        className="h-[32rem] w-[32rem]"
+      />
+
+      <div className="flex w-full flex-col items-center text-center">
+        <p className="head1 mb-[3.1rem] text-black">
+          다람쥐 치삐와 함께
+          <br />
+          도토리를 모아볼까요?
+        </p>
+        <div>
+          <Button variant="primary" onClick={() => {}}>
+            지금 시작하기
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FinalCTASection;
