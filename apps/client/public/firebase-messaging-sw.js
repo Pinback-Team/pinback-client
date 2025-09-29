883c4865da08a1cfbf386b7aadd5cdd54bc9b3fa
jllee000 <jllee000@naver.com>
2025-09-30 00:04:31 +0900

    feat: 키 연결 및 cdn 임포트

--- apps/client/public/firebase-messaging-sw.js
+++ apps/client/public/firebase-messaging-sw.js
@@ -1,10 +1,21 @@
 /* eslint-env serviceworker */
 /* eslint-disable no-undef */
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyD3KM0IQ4Ro3Dd2fyAY8fnhE1bQ_NesrBc',
+  authDomain: 'pinback-c55de.firebaseapp.com',
+  projectId: 'pinback-c55de',
+  storageBucket: 'pinback-c55de.firebasestorage.app',
+  messagingSenderId: '370851215931',
+  appId: '1:370851215931:web:08382b5e57808d29dcba1e',
+  measurementId: 'G-847ZNSCC3J',
+};
+
 importScripts(
-  './firebase_sdk/firebase-app-compat.js',
-  './firebase_sdk/firebase-messaging-compat.js',
-  './firebase-config.js'
+  'https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js'
+);
+importScripts(
+  'https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js'
 );
 
 self.addEventListener('install', function () {
@@ -16,6 +27,7 @@ self.addEventListener('activate', function () {
 });
 
 firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
