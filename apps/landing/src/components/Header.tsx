b18b21fc57371cb0c0f917bb8b803783b02acbff
jjangminii <wjdals030328@catholic.ac.kr>
2025-10-07 23:21:04 +0900

    chore: 주석제거

--- apps/landing/src/components/Header.tsx
+++ apps/landing/src/components/Header.tsx
@@ -6,7 +6,6 @@ const Header = () => {
     <header className="fixed left-0 right-0 top-0 z-50 flex h-[7.4rem] w-full items-center justify-between px-[8rem]">
       <Icon name={'logo'} width={87} height={24} />
       <div>
-        {/*TODO: onClick 추가 */}
         <Button
           onClick={() => {
             window.location.href =
