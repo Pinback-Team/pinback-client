b747e01c829643ffd109bd800c90d25897dc4558
jjangminii <wjdals030328@catholic.ac.kr>
2025-11-19 18:29:03 +0900

    feat: 소셜로그인 페이지 퍼블리싱

--- apps/client/src/pages/onBoarding/components/funnel/step/SocialLoginStep.tsx
+++ apps/client/src/pages/onBoarding/components/funnel/step/SocialLoginStep.tsx
@@ -1,16 +1,36 @@
+import Chippi from '@assets/chippi_extension_popup.svg';
+import GoogleLogo from '@assets/onBoarding/icons/googleLogo.svg';
+
 const SocialLoginStep = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h2 className="mb-4 text-lg font-bold">소셜 로그인해주세요</h2>
+    <div className="flex flex-col items-center justify-center pt-6">
+      <img
+        src={Chippi}
+        alt="치삐 이미지"
+        className="h-[19.4rem] w-[19.4rem] object-contain"
+      />
 
+      <h1 className="head2 text-font-black-1 mb-[0.8rem] text-center">
+        치삐를 만나려면 로그인이 필요해요!
+      </h1>
+
+      <p className="body2-m text-font-gray-3 mb-[3.5rem] text-center">
+        로그인하고 북마크한 정보를 리마인드를 받아보세요.
+      </p>
+
+      {/* 구글 로그인 버튼 */}
       <button
         onClick={() => {
-          // 원래 네 소셜 로그인 플로우로 이동하도록 수정
           window.location.href = `/social-login?nextStep=4`;
         }}
-        className="rounded bg-blue-500 px-4 py-2 text-white"
+        className="body1-m flex h-[52px] w-[22.7rem] items-center justify-between gap-3 rounded-full border border-gray-100 bg-white px-[2rem]"
       >
-        구글 로그인
+        <img
+          src={GoogleLogo}
+          alt="구글 로고"
+          className="h-[2.435rem] w-[2.435rem]"
+        />
+        구글 계정으로 로그인
       </button>
     </div>
   );
