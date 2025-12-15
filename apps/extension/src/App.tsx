dacf9e3091cc65746b6ecf184f24700b5f92bdc7
jllee000 <jllee000@naver.com>
2025-12-15 18:09:03 +0900

    feat: 로그아웃 팝업 창 작엉ㅂ

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -1,41 +1,43 @@
 import './App.css';
-import DuplicatePop from './pages/DuplicatePop';
-import MainPop from './pages/MainPop';
-import { useState, useEffect } from 'react';
-import { useGetArticleSaved } from '@apis/query/queries';
-import { usePageMeta } from './hooks/usePageMeta';
+// import DuplicatePop from './pages/DuplicatePop';
+// import MainPop from './pages/MainPop';
+// import { useState, useEffect } from 'react';
+// import { useGetArticleSaved } from '@apis/query/queries';
+// import { usePageMeta } from './hooks/usePageMeta';
+import LogOutPop from './pages/LogOutPop';
 const App = () => {
-  const { url } = usePageMeta();
-  const { data: isSaved } = useGetArticleSaved(url);
+  // const { url } = usePageMeta();
+  // const { data: isSaved } = useGetArticleSaved(url);
 
-  const [isDuplicatePop, setIsDuplicatePop] = useState(false);
-  const [mainPopType, setMainPopType] = useState<'add' | 'edit'>('add');
+  // const [isDuplicatePop, setIsDuplicatePop] = useState(false);
+  // const [mainPopType, setMainPopType] = useState<'add' | 'edit'>('add');
 
-  useEffect(() => {
-    if (isSaved?.data) {
-      setIsDuplicatePop(true);
-    }
-  }, [isSaved]);
+  // useEffect(() => {
+  //   if (isSaved?.data) {
+  //     setIsDuplicatePop(true);
+  //   }
+  // }, [isSaved]);
 
-  const handleDuplicateLeftClick = () => {
-    setIsDuplicatePop(false);
-    setMainPopType('edit');
-  };
+  // const handleDuplicateLeftClick = () => {
+  //   setIsDuplicatePop(false);
+  //   setMainPopType('edit');
+  // };
 
-  const handleDuplicateRightClick = () => {
-    chrome.tabs.create({ url: 'https://www.pinback.today/' });
-  };
+  // const handleDuplicateRightClick = () => {
+  //   chrome.tabs.create({ url: 'https://www.pinback.today/' });
+  // };
 
   return (
     <>
-      {isDuplicatePop ? (
+      <LogOutPop />
+      {/* {isDuplicatePop ? (
         <DuplicatePop
           onLeftClick={handleDuplicateLeftClick}
           onRightClick={handleDuplicateRightClick}
         />
       ) : (
         <MainPop type={mainPopType} savedData={isSaved?.data} />
-      )}
+      )} */}
     </>
   );
 };
