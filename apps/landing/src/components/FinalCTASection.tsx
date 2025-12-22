aa8f931bbecb540ed81d6e5006d5fc207e8cd048
이진혁 <rntqhdl10@naver.com>
2025-12-22 11:05:41 +0900

    feat(client, landing): footer 구현 (#220)

--- apps/landing/src/components/FinalCTASection.tsx
+++ apps/landing/src/components/FinalCTASection.tsx
@@ -1,6 +1,7 @@
 import Lottie from 'lottie-react';
 import Chippiface from '../assets/5_chippiface.json';
 import { sendGAEvent, Button } from '@pinback/design-system/ui';
+import Footer from './Footer';
 const FinalCTASection = () => {
   const handleInstallClick = () => {
     sendGAEvent('landing', 'landing', '2-landing-bottomBtn');
@@ -9,27 +10,30 @@ const FinalCTASection = () => {
   };
 
   return (
-    <section className="bg-white-bg flex h-dvh w-full flex-col items-center justify-center gap-[6.4rem] overflow-hidden px-[17.2rem] text-center">
-      <Lottie
-        animationData={Chippiface}
-        loop
-        autoplay
-        className="h-[32rem] w-[32rem]"
-      />
+    <div className="relative flex h-dvh flex-col">
+      <section className="bg-white-bg mt-[10.3rem] flex w-full flex-col items-center justify-center gap-[2rem] overflow-hidden px-[17.2rem] text-center">
+        <Lottie
+          animationData={Chippiface}
+          loop
+          autoplay
+          className="h-[32rem] w-[32rem]"
+        />
 
-      <div className="flex w-full flex-col items-center text-center">
-        <p className="head1 mb-[3.1rem] text-black">
-          다람쥐 치삐와 함께
-          <br />
-          도토리를 모아볼까요?
-        </p>
-        <div>
-          <Button variant="primary" onClick={handleInstallClick}>
-            지금 시작하기
-          </Button>
+        <div className="flex w-full flex-col items-center text-center">
+          <p className="head1 mb-[3.2rem] text-black">
+            다람쥐 치삐와 함께
+            <br />
+            도토리를 모아볼까요?
+          </p>
+          <div>
+            <Button variant="primary" onClick={handleInstallClick} size="large">
+              지금 시작하기
+            </Button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Footer />
+    </div>
   );
 };
 
