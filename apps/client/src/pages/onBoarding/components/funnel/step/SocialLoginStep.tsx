2fe37f44ce9b9ed4cf916e8481968c52126f69d8
jjangminii <wjdals030328@catholic.ac.kr>
2025-12-29 03:00:00 +0900

    fix: Google 로그인 리디렉션 URI 설정 로직 수정

--- apps/client/src/pages/onBoarding/components/funnel/step/SocialLoginStep.tsx
+++ apps/client/src/pages/onBoarding/components/funnel/step/SocialLoginStep.tsx
@@ -4,13 +4,16 @@ import GoogleLogo from '/assets/onBoarding/icons/googleLogo.svg';
 const SocialLoginStep = () => {
   const handleGoogleLogin = () => {
     const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-    const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
+    // const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
+
+    const redirectUri = import.meta.env.PROD
+      ? import.meta.env.VITE_GOOGLE_REDIRECT_URI_PROD
+      : import.meta.env.VITE_GOOGLE_REDIRECT_URI_DEV;
 
     if (!clientId || !redirectUri) {
       alert('Google OAuth 설정이 누락되었습니다.');
       return;
     }
-
     const googleAuthUrl =
       `https://accounts.google.com/o/oauth2/v2/auth?` +
       `client_id=${clientId}` +
