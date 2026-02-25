2fa352d57af6dd36e192970ebdf06f7e66129b9d
constantly-dev <rntqhdl10@naver.com>
2026-02-25 21:10:42 +0900

    feat: auth signup end point v3로 변경

--- apps/client/src/shared/apis/axios.ts
+++ apps/client/src/shared/apis/axios.ts
@@ -33,10 +33,11 @@ export interface postSignUpRequest {
   email: string;
   remindDefault: string;
   fcmToken: string | null;
+  job: string;
 }
 
 export const postSignUp = async (responsedata: postSignUpRequest) => {
-  const { data } = await apiRequest.patch('/api/v2/auth/signup', responsedata);
+  const { data } = await apiRequest.patch('/api/v3/auth/signup', responsedata);
   return data;
 };
 
