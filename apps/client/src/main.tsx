627c0e664584e37f0244028707fe5c625f6a7f15
constantly-dev <rntqhdl10@naver.com>
2026-03-28 02:02:32 +0900

    setting: client, extension initAnalytics 추가

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,12 @@
+import { initAnalytics } from '@pinback/analytics';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+
+initAnalytics({
+  apiKey: import.meta.env.VITE_AMPLITUDE_API_KEY,
+  isDev: import.meta.env.DEV,
+});
 import { QueryClientProvider } from '@tanstack/react-query';
 import getQueryClient from '@shared/apis/setting/query/getQueryClient.ts';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
