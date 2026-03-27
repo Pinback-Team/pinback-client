e3b5c9a8e05ed3cf00f6e14de4e34215d028d53d
constantly-dev <rntqhdl10@naver.com>
2026-03-27 16:52:30 +0900

    refactor: consoleProvider init apiKey 종속 제거

--- packages/analytics/src/providers/console.ts
+++ packages/analytics/src/providers/console.ts
@@ -1,7 +1,7 @@
 import type { AnalyticsProvider } from '../types';
 
 export const consoleProvider: AnalyticsProvider = {
-  init: (apiKey) => console.log('[Analytics] init', { apiKey }),
+  init: (_apiKey) => console.log('[Analytics] init'),
   track: (name, props) => console.log('[Analytics] track', name, props ?? {}),
   identify: (userId) => console.log('[Analytics] identify', { userId }),
   reset: () => console.log('[Analytics] reset'),
